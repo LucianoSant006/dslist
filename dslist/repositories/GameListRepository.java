aa3d8db78a007e4c47fee5d006f25d9629ea6585
LucianoSantiago <131803579+LucianoSant006@users.noreply.github.com>
2024-08-11 11:23:57 -0300

    Delete dslist directory

--- dslist/repositories/GameListRepository.java
+++
@@ -1,9 +0,0 @@
-package com.LucianoSant.dslist.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.LucianoSant.dslist.entites.GameList;
-
-public interface GameListRepository extends JpaRepository<GameList,Long> {
-
-}
